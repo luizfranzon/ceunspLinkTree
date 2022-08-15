a41b981754a72409145933e065e081afc345ef48
Luiz Franzon <luizfranzon05@hotmail.com>
2022-08-15 15:07:40 -0300

    Added other buttons

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -1,8 +1,24 @@
 import { Header } from "./components/Header";
 
+import styles from "./App.module.css";
+import blackBoardLogo from "./assets/blackBoardIcon.png";
+import carteirinhaLogo from "./assets/carteirinhaLogo.png";
+
 export function App() {
 
     return (
-        <Header/>
+        <>
+            <Header/>
+            <section>
+                <main className={styles.main}>
+                    <a className={styles.button} target="_blank" href="https://www.ceunsp.edu.br/aluno/"><button>Site da Instituição</button></a>
+                    <a className={styles.button} target="_blank" href="https://novoportal.cruzeirodosul.edu.br/?empresa=ceunsp"><button>Portal do Aluno</button></a>
+                    <a className={styles.blackBoardButton} target="_blank" href="https://bb.cruzeirodosulvirtual.com.br/"><button> <img height={60} src={blackBoardLogo} alt="" /> Black Board</button></a>
+                    <a className={styles.carteirinhaButton} target="_blank" href="https://play.google.com/store/apps/details?id=com.inovatecnologia.carteirinha&hl=pt&gl=US"><button> <img height={40} src={carteirinhaLogo} alt="" /> Carteirinha</button></a>
+                </main>
+
+                <footer>Desenvolvido por <a href="https://github.com/luizfranzon">Luiz Franzon</a></footer>
+            </section>
+        </>
     )
 }
--- App/src/components/Header/index.jsx
+++ App/src/components/Header/index.jsx
@@ -1,11 +1,12 @@
 import styles from "./Header.module.css"
 
-import logo from "../../assets/logo.png"
+import logo from "../../assets/CeunspLogo.png"
 
 export function Header() {
     return (
         <header className={styles.header}>
-            <img src={logo} alt="" />
+            <img src={logo} alt="Logo da Ceunsp. Uma estrela vermelha, e um texto azul escrito Ceunsp" />
+            <h1>Link Tree</h1>
         </header>
     )
 }
