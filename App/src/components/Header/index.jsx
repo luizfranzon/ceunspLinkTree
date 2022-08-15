2b788ef48d6c751675d05cc0ab8bb6facbaf4cbd
Luiz Franzon <luizfranzon05@hotmail.com>
2022-08-15 15:14:03 -0300

    header logo

--- App/src/components/Header/index.jsx
+++ App/src/components/Header/index.jsx
@@ -1,11 +1,11 @@
 import styles from "./Header.module.css"
 
-import logo from "../../assets/CeunspLogo.png"
+import logoCeunsp from "../../assets/Ceunsplogo.png"
 
 export function Header() {
     return (
         <header className={styles.header}>
-            <img src={logo} alt="Logo da Ceunsp. Uma estrela vermelha, e um texto azul escrito Ceunsp" />
+            <img src={logoCeunsp} alt="Logo da Ceunsp. Uma estrela vermelha, e um texto azul escrito Ceunsp" />
             <h1>Link Tree</h1>
         </header>
     )
